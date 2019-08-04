511b4282f46852acaf8d5cb73fa8aaf3f22aa239
quanht <zed.quan99@gmail.com>
2019-08-04 17:48:23 +0700

    init project and create model User

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const BodyParser = require("body-parser");
+const morgan = require("morgan");
+const config = require("config");
+const cors = require("cors");
+const port = config.get("port");
+
+const db = require("./model/database");
+
+const app = express();
+app.use(cors());
+app.use(BodyParser.urlencoded({extended: false}));
+app.use(BodyParser.json());
+app.use(morgan("tiny"));
+
+app.use("/ping", (req, res) => {
+    res.send("pong")
+});
+
+db.sequelize.sync({
+    force: false
+})
+    .then(() => {
+        app.listen(port, (err) => {
+            if (!err) console.log("Server running on ", port);
+        });
+
+    })
+    .catch(err => {
+        console.log(err.message)
+    });
+
