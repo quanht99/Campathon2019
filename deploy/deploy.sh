e393f61a7d558969eb1d03150feff2ff0ea7aedc
Tien Quan <zed.quan99@gmail.com>
2020-08-01 16:35:43 +0700

    update script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,5 @@
 if [[ "$1" = "production" ]]
 then
     rsync -delete -e "ssh -i quanht.pem -o StrictHostKeyChecking=no" ./ quanht@34.87.80.48:/home/quanht/$2
-    ssh -i quanht.pem -o StrictHostKeyChecking=no quanht@34.87.80.48 'bash -s' < updateAndRestart.sh $nodeHome $2
+    ssh -i quanht.pem -o StrictHostKeyChecking=no quanht@34.87.80.48 'bash -s' < ./deploy/updateAndRestart.sh $nodeHome $2
 fi
